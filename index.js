3ee4a18296f2f8b167db7cf13aca2621d14b4b54
Carlos Pablo <isccpandrade@gmail.com>
2020-09-14 11:40:59 -0500

    cambio de puerto

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser')
+const cors = require('cors');
+
     // INICIALIZAR VARIABLES
 
 var app = express();
@@ -12,13 +14,17 @@ var app = express();
 const port = process.env.PORT || 4002;
 //CORS
 
+/*
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE, OPTIONS")
     next();
 });
+*/
 
+//HABILITAR CORS
+app.use(cors());
 
 // BODY PARSER
 // parse application/x-www-form-urlencoded
